9a5221ffe2a8cf9a0e1b393ae24085fa3b08b707
Nina Ebensperger <112419165+ninae555@users.noreply.github.com>
2024-03-06 18:43:09 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Install PyTorch
-pip install torch==1.10.0+cu113 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch==1.10.0
 
 # Install torch-scatter and torch-sparse
-pip install torch-scatter -f https://data.pyg.org/whl/torch-1.10.0+cu113.html
-pip install torch-sparse -f https://data.pyg.org/whl/torch-1.10.0+cu113.html
+pip install torch-scatter -f https://data.pyg.org/whl/torch-1.10.0+cpu.html
+pip install torch-sparse -f https://data.pyg.org/whl/torch-1.10.0+cpu.html
 
 # Install PyTorch Geometric
 pip install git+https://github.com/pyg-team/pytorch_geometric.git
@@ -14,4 +14,4 @@ pip install git+https://github.com/pyg-team/pytorch_geometric.git
 git clone https://github.com/pmaldonado/cs224w-project-data.git
 
 # Install packages from requirements.txt
-pip install -r requirements.txt
+pip install -r environments/requirements.txt
